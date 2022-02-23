c44a91a87a97ad155cad2dbcad6cf87371d766c4
yJon4ss <motajoao670@gmail.com>
2022-02-23 20:56:48 +0000

    .

--- src/events/client/clientEvents/ready.js
+++ src/events/client/clientEvents/ready.js
@@ -4,7 +4,7 @@ module.exports = async(client) => {
   console.log(`[ SHARDS ] › Shard ${client.shard.ids} conectada!`.green)
   await delay(5000)
   client.music.start(client.user.id);
-  client.user.setActivity(`<help - ${await client.shard.fetchClientValues("guilds.cache.size").then((x) => x.reduce((f, y) => f + y))} Guilds | [${client.shard.id}]`, {
+  client.user.setActivity(`<help - ${await client.shard.fetchClientValues("guilds.cache.size").then((x) => x.reduce((f, y) => f + y))} Guilds | [${client.shard.ids}]`, {
     type: 'LISTENING',
   })
 };

a966a48dc45245e60c95749f3967098b4f551ec1
yJon4ss <motajoao670@gmail.com>
2022-03-02 14:59:33 +0000

    :recycle: Making a filter on the process events

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-require("colors");
-
+require('colors');
 const yaml = require('js-yaml');
 const { readFileSync } = require('fs');
-
 global.yml = yaml.load(readFileSync('./env.yml', 'utf8'));
 
-const { connect } = require("mongoose")
+const { connect } = require('mongoose')
 connect(yml.database, { useNewUrlParser: true, useUnifiedTopology: true }).catch(console.error)
 
 const SiestaClient = require('./Structures/SiestaClient');
@@ -13,14 +11,9 @@ const client = new SiestaClient();
 
 client.login();
 
-process.on("unhandledRejection", (reason, p) => {
-    if (String(reason).includes("Shards are still being spawned") || String(reason).includes("Missing Permissions")) return;
-    console.log("\n\n");
-    console.log(String(reason.stack).gray);
-    console.log("\n\n");
-  });
-  process.on("uncaughtException", (err, origin) => {
-    console.log("\n\n");
-    console.log(String(err.stack).gray);
-    console.log("\n\n");
-  })
+const filter = (err) => { 
+  if(!err.toString().toLowerCase().includes('missing permissions') || !err.toString().toLowerCase().includes('missing acess')) console.log(String(err.stack).gray) 
+}
+
+process.on('unhandledRejection', filter)
+process.on('uncaughtException', filter)

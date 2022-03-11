143dbb58db11ff6fd186fee9aba12ea0ba60e7a9
yJon4ss <motajoao670@gmail.com>
2022-03-11 21:22:36 +0000

    :package: Updating vulkava to latest version.

--- src/commands/Owner/nodes.js
+++ src/commands/Owner/nodes.js
@@ -6,7 +6,7 @@ module.exports = {
   ownerOnly: true,
   run: async (client, message, args, player, lang) => {
 
-    const map = client.music.nodes.map(node => `[ Node ${node.options.id}] Players: ${node.stats.players} | ${node.stats.playingPlayers} Uptime: ${client.utils.formatTime(node.stats.uptime)}, State: ${String(node.state).replace('0', 'CONNECTING').replace('1', 'CONNECTED').replace('2', 'DISCONNECTED')}`)
+    const map = client.music.nodes.map(node => `[ Node ${node.options.id} ] Players: ${node.stats.players}  ${node.stats.playingPlayers} Uptime: ${client.utils.formatTime(node.stats.uptime)}, State: ${String(node.state).replace('0', 'CONNECTING').replace('1', 'CONNECTED').replace('2', 'DISCONNECTED')}`)
     message.reply({ content: `\`\`\`\n${map.join('\n')}\n\`\`\`` });
   },
 };

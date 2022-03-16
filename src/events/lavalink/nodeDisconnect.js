a249f6941e3e7132f04c005d8b2c00ce2b4ed0f3
yJon4ss <motajoao670@gmail.com>
2022-03-16 20:18:44 +0000

    resolved a bug.

--- src/events/lavalink/nodeDisconnect.js
+++ src/events/lavalink/nodeDisconnect.js
@@ -1,3 +1,3 @@
-module.exports = async (client, node, reason) => {
+module.exports = async (client, node, code, reason) => {
   console.log(`Node ${node.options.id} desconectado \n ${String(reason)}`.gray);
 };

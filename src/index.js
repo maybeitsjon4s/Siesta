c6c8fefefd27e7297f2c93a58690918a21fb46f7
yJon4ss <motajoao670@gmail.com>
2022-04-09 18:04:56 +0100

    added topgg autoposter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 const { load } = require('js-yaml');
 const { readFileSync } = require('fs');
 global.config = load(readFileSync('./config.yml', 'utf8'));
-
 const { connect } = require('mongoose');
-
-connect(global.config.database).then(() => {}).catch(console.error);
+const { AutoPoster } = require('topgg-autoposter')
+connect(global.config.database).catch(console.error);
 
 const SiestaClient = require('./Structures/SiestaClient');
 const client = new SiestaClient();
 
 client.start();
-
+AutoPoster(global.config.topggToken, client)
 const filter = (err) => { 
   if(!['Missing Access', 'Missing Permissions'].includes(err)) console.log('\n\n'); client.logger.stack(err.stack); 
 };
-
 global.process.on('unhandledRejection', filter);
 global.process.on('uncaughtException', filter);

e455bbde460204ee8af4cac9dfa48c0d30753922
yJon4ss <motajoao670@gmail.com>
2022-04-10 14:12:45 +0100

    Errors...

--- src/commands/Configs/autorole.js
+++ src/commands/Configs/autorole.js
@@ -8,7 +8,7 @@ module.exports = {
   ownerOnly: false,
   async exec({ client, message, args, lang }) {
 
-    if(!message.member.permissions.has('MANAGE_GUILD')) return message.reply(`**${Emojis.errado} › **`);
+    if(!message.member.permissions.has('MANAGE_GUILD') && !client.owners.some(id => id === message.author.id)) return message.reply(`**${Emojis.errado} › ${lang.commands.autorole.errorPerm}**`)
     if(!args[0] || !['status', 'role'].includes(args[0]) || args[0] == 'help') return message.reply({ embeds: [{
       color: client.color,
       title: Emojis.config + '__Siesta__',

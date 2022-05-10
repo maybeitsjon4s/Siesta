6aeca883d3f6244e3ae4e9c13109d201d94d4fb3
yJon4ss <motajoao670@gmail.com>
2022-05-10 20:51:58 +0100

    fix: missing one arg in getUser function

--- src/Commands/Mod/ban.js
+++ src/Commands/Mod/ban.js
@@ -28,7 +28,7 @@ module.exports = {
     const motivo = args.slice(1).join(' ');
     if (!args[0]) return message.reply(`**${Emojis.errado} › ${t('commands:ban.noMention')}**`);
        
-    const user = await client.utils.getUser(args[0]);
+    const user = await client.utils.getUser(args[0], message);
 
     if(!user) return message.reply(`**${Emojis.errado} › ${t('commands:ban.invalidUser')}**`);
 

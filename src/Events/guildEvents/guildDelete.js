8292f2e1c1a3c3571d862918b2cefe48b93cb436
yJon4ss <motajoao670@gmail.com>
2022-05-16 19:29:57 +0100

    fix: guildDelete

--- src/Events/guildEvents/guildDelete.js
+++ src/Events/guildEvents/guildDelete.js
@@ -5,7 +5,7 @@ export default {
   async exec (client, guild) {
     client.utils.sendLogs({
       type: 'guild',
-      content: `\`---\`Data: **${Day(Date.now()).format('DD/MM/YYYY HH:mm:ss')}**\nFui removida do servidor: **${guild.name}** (\`${guild.id}\`)\nMembros: **${guild.memberCount}\n\`---\``
+      content: `\`---\`\nData: **${Day(Date.now()).format('DD/MM/YYYY HH:mm:ss')}**\nFui removida do servidor: **${guild.name}** (\`${guild.id}\`)\nMembros: **${guild.memberCount}**\n\`---\``
     });
     await client.db.guild.findOneAndDelete({ _id: guild.id }).catch(() => {});
   }

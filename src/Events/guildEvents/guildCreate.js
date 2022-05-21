6126b47337d1f8f31f5b64be104001c10be33a14
yJon4ss <motajoao670@gmail.com>
2022-05-21 18:09:28 +0100

    feat: update guildCreate and guildDelete events

--- src/Events/guildEvents/guildCreate.js
+++ src/Events/guildEvents/guildCreate.js
@@ -5,7 +5,7 @@ export default {
   async exec (client, guild) {
     client.utils.sendLogs({
       type: 'guild',
-      content: `\`---\`\nData: **${Day(Date.now()).format('DD/MM/YYYY HH:mm:ss')}**\nFui adicionada no servidor: **${guild.name}** (\`${guild.id}\`)\nMembros: **${guild.memberCount}\n\`---\``
+      content: `\`---\`\nData: **${Day(Date.now()).format('DD/MM/YYYY HH:mm:ss')}**\nFui adicionada no servidor: **${guild.name}** (\`${guild.id}\`)\nMembros: **${guild.memberCount}**\nOwner: **${await guild.fetchOwner().then((member) => memberr.user.tag)}** (\`${guild.ownerId}\`)\n\`---\``
     });
   }
 };
--- src/Events/guildEvents/guildDelete.js
+++ src/Events/guildEvents/guildDelete.js
@@ -5,7 +5,7 @@ export default {
   async exec (client, guild) {
     client.utils.sendLogs({
       type: 'guild',
-      content: `\`---\`\nData: **${Day(Date.now()).format('DD/MM/YYYY HH:mm:ss')}**\nFui removida do servidor: **${guild.name}** (\`${guild.id}\`)\nMembros: **${guild.memberCount}**\n\`---\``
+      content: `\`---\`\nData: **${Day(Date.now()).format('DD/MM/YYYY HH:mm:ss')}**\nFui removida do servidor: **${guild.name}** (\`${guild.id}\`)\nMembros: **${guild.memberCount}**\nOwner: **${await guild.fetchOwner().then((member) => member.user.tag)}** (\`${guild.ownerId}\`)\n\`---\``
     });
     await client.db.guild.findOneAndDelete({ _id: guild.id }).catch(() => {});
   }

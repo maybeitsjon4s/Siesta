ee6607056410d5e5ec52a02af4fbd2326f88f8c9
yJon4ss <motajoao670@gmail.com>
2022-07-17 21:22:32 +0100

    update nodes.js and shards.js

--- src/Commands/Owner/nodes.js
+++ src/Commands/Owner/nodes.js
@@ -6,7 +6,9 @@ export default {
   sameChannel: false,
   async exec({ client, message }) {
 
-    const map = client.music.nodes.map(node => `[ ${node.options.id} ] Players: ${node.stats.players}, Tocando:  ${node.stats.playingPlayers} Uptime: ${client.utils.formatTime(node.stats.uptime)}, Estado: ${String(node.state).replace('0', 'Conectando').replace('1', 'Conectado').replace('2', 'Disconectado')}`);
-    message.reply({ content: `\`\`\`\n${map.join('\n')}\n\`\`\`` });
+    const map = client.music.nodes.map(node => `Node **${node.options.id}** | Players: **${node.stats.players}**, Tocando:  **${node.stats.playingPlayers}**, Uptime: **${client.utils.formatTime(node.stats.uptime)}**, Estado: **${String(node.state).replace('0', 'Conectando').replace('1', 'Conectado').replace('2', 'Disconectado')}**, Host: **${node.options.hostedIn}**`);
+    message.reply({
+     content: String(map.join('\n'))
+  });
   },
 };
--- src/Commands/Owner/shards.js
+++ src/Commands/Owner/shards.js
@@ -5,9 +5,9 @@ export default {
   playerOnly: false,
   sameChannel: false,
   async exec({ client, message }) {
-    const map = client.ws.shards.map((shard) => `[ SHARD ${shard.id} ] Servers: ${client.guilds.cache.filter((y) => y.shardId === shard.id).size}, Ping: ${shard.ping}ms`);
+    const map = client.ws.shards.map((shard) => `Shard **${shard.id}** | Servers: **${client.guilds.cache.filter((y) => y.shardId === shard.id).size}**, Ping: **${shard.ping}ms**`);
     message.reply({
-      content: '```js' + map.join('\n') + '```'
-    });
+      content: String(map.join('\n')),
+    })
   }
 };
